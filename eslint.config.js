2cce4a0a04f4693b64b608170edafe6bfedc506a
Joey.Essak <javascriptjoey@outlook.com>
2025-08-02 18:34:13 -0700

    Initial commit: Vite React project with Storybook setup
    ✨ Features:
    - React 19 + TypeScript + Vite setup
    - Storybook integration with example components
    - Vitest testing setup with coverage
    - ESLint + Prettier configuration
    - Tailwind CSS for styling
    - Cool ASCII art README 🫧

---
+++ eslint.config.js
@@ -0,0 +1,39 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { globalIgnores } from 'eslint/config'
+import prettier from 'eslint-plugin-prettier'
+
+
+export default tseslint.config([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+      'plugin:prettier/recommended',
+    ],
+    plugins: {
+      prettier,
+    },
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    rules: {
+      'prettier/prettier': ['error', { semi: true, singleQuote: true, tabWidth: 2 }],
+      'no-unused-vars': 'warn',
+      'no-undef': 'error',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+    },
+  },
+], storybook.configs["flat/recommended"]);
